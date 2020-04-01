eafcd2b19ff4c0e5fcf1470d86099ad6aac1819f
luisfigueroaa <luis_figueroa_morales@yahoo.com>
2020-04-01 14:39:56 -0500

    Update getting input from user.py

--- getting input from user.py
+++ getting input from user.py
@@ -1,4 +1,4 @@
 nombre = input('Ingrese su nombre: ')
 edad = input('Ingrese su edad: ')
 print("Hola " + nombre + ", bienvenido!!")
-print("Tienes " + edad + " añosL")
+print("Tienes " + edad + " años")
